d1a70c491ee148ecaa58fd1b7cb8f939d98f108c
Hiago dos Santos <dev.hisan@gmail.com>
2020-09-23 22:21:32 -0300

    fix aws workflow cluster name, disable tailwind warns

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
+  // Disable WARNs on build proccess
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
   prefix: 'tw-',
   separator: '--',
   purge: false,
