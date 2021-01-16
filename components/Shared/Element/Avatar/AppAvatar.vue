20a102519e3ef4276dc66ab50d2b12bbe037ba98
Hiago dos Santos <dev.hisan@gmail.com>
2021-01-16 13:28:16 -0300

    Added Avatar

---
+++ components/Shared/Element/Avatar/AppAvatar.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .avatar
+    .inner
+      img(:src="image")
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'AppAvatar',
+  props: {
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
