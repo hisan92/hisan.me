ae9d87f39e7497ccfc8a08dc8da5442d0abf9418
Hiago dos Santos <dev.hisan@gmail.com>
2021-01-16 15:03:07 -0300

    Added Divider

---
+++ components/Shared/Element/Divider/AppDivider.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .divider(:class="{ vertical: vertical }")
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'AppDivider',
+  props: {
+    vertical: {
+      type: Boolean,
+      default: () => false,
+    },
+  },
+})
+</script>
